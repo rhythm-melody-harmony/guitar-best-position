51c8e505097dd25b2b4c1875e28ea8ba5105e8f2
Dejan Radovanović <deradovanovic@gmail.com>
2017-05-28 23:26:34 +0200

    JUnit test fixed

--- src/test/java/com/rmh/guitar/bestposition/fretboard/settings/DefaultFretBoardSettingsFactoryTest.java
+++ src/test/java/com/rmh/guitar/bestposition/fretboard/settings/DefaultFretBoardSettingsFactoryTest.java
@@ -1,20 +1,38 @@
 package com.rmh.guitar.bestposition.fretboard.settings;
 
+import com.rmh.guitar.bestposition.GuitarBestPositionApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import org.junit.Test;
-
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(properties = {"guitar.setup.number-of-frets=17", "guitar.setup.number-of-strings=4"})
 public class DefaultFretBoardSettingsFactoryTest {
+
+	@Autowired
+	private DefaultFretBoardSettingsFactory defaultFretBoardSettingsFactory;
 
 	@Test
 	public void testCreate() {
-		DefaultFretBoardSettingsFactory defaultFretBoardSettingsFactory = new DefaultFretBoardSettingsFactory();
-		
 		FretBoardSettings fretBoardSettings = defaultFretBoardSettingsFactory.create();
+
+		log.info("fretBoardSettings: {}", fretBoardSettings);
 		
 		assertNotNull(fretBoardSettings);
-		assertNotNull(fretBoardSettings.getNumberOfFrets());
-		assertNotNull(fretBoardSettings.getNumberOfStrings());
+		assertEquals(17, fretBoardSettings.getNumberOfFrets());
+		assertEquals(4, fretBoardSettings.getNumberOfStrings());
 		assertNotNull(fretBoardSettings.getTuning());
 	}
 
